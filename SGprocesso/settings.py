2d6ef5efde92400e27577c5c6dc8618edc018f2f
ismaely <7ilipe@gmail.com>
2022-06-24 15:39:53 +0100

    mudança ni ficheiro de configuração

--- SGprocesso/settings.py
+++ SGprocesso/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 ROOT_URLCONF = 'SGprocesso.urls'
 
@@ -128,18 +129,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 #if not DEBUG:
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+#STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 #else:
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static_cdn/'),
+    os.path.join(BASE_DIR, 'static'),
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
